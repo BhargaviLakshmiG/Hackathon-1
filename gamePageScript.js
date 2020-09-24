cac827f0190fbfbde1b26c26fd2fa94906b682f4
BhargaviLakshmiG <70680903+BhargaviLakshmiG@users.noreply.github.com>
2020-09-24 17:44:13 +0530

    Add files via upload
    Updated gamescript.js with alphabets added

--- gamePageScript.js
+++ gamePageScript.js
@@ -2,10 +2,59 @@
 var wrap=document.createElement("div");
 wrap.setAttribute("id","wrapper");
 var h=document.createElement("h1");
-h.innerHTML="Welcome to HangMan";
+h.innerHTML="HANGMAN";
+
+var hmimg=document.createElement("img");
+hmimg.setAttribute("src","first.jpg")
 
 var text=document.createElement("h3");
-text.innerHTML="Use the alphabets below to guess the word ";
+text.innerHTML="GUESS THE BELOW WORD ";
 
-wrap.append(h,text);
+var alphlist=document.createElement("div");
+alphlist.setAttribute("id","alphabets");
+//alphlist.setAttribute("value","buttons")
+//alphlist.append(buttons)
+wrap.append(h,hmimg,text,alphlist);
+
 document.body.append(wrap);
+
+
+
+
+var words_list= [
+	      "awkward",
+	      "avenge",
+	      "backstab",
+	      "control",
+	      "dream",
+	      "future",
+	      "gentle",
+	      "happy",
+	      "iguana",
+	      "jelly",
+	      "mountain",
+	      "smile",
+	      "tribute",
+	      "venture"
+]
+
+
+let answer = '';
+let maxWrong = 6;
+let mistakes = 0;
+
+function randomWord() {
+  answer = words_list[Math.floor(Math.random() * words_list.length)];
+}
+
+function generateButtons() {
+  let buttonsHTML = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter =>
+    `
+      <button id='` + letter + `'>` + letter + `</button>`).join('');
+
+  document.getElementById('alphabets').innerHTML = buttonsHTML;
+}
+
+
+
+generateButtons();
